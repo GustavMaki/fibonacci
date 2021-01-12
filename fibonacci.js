dce54e16f38b4ae19b420fcf1cc0f72164e167c6
GustavMaki <gustav.siljamaki@gmail.com>
2021-01-12 14:44:23 +0100

    v.3

--- fibonacci.js
+++ fibonacci.js
@@ -1,11 +1,22 @@
-var i= 2; var fibo = [];
+const readline = require('readline');
+var i= 2; var seq = [];
 
-fibo[0]= 0; 
-fibo[1]= 1;
+seq[0]= 0; 
+seq[1]= 1;
 
-while(i < 50){
 
-    fibo[i] = fibo[i-1] + fibo[i-2];
-    console.log(fibo[i]);
-    i++
-}
+const input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+input.question('Please enter amount: ', (answer) => {
+
+    while(i < answer){
+        seq[i] = seq[i-1] + seq[i-2];
+        console.log((i+1)+ ". " + seq[i-2]);
+        i++
+    }
+
+    input.close();
+});
